287fca24b565b30f5d2318967e761cdc33c02503
Viktor <149080919+Viktor672@users.noreply.github.com>
2025-03-16 15:55:32 +0200

    Installing React Router for navigaiton

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
-import './App.css'
+import './App.css';
+import { Routes, Route } from 'react-router';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+
 
 function App() {
   return (
-    <>
-    </>
+    <div id="box">
+      <Header />
+
+      <main id="main-content">
+        <Routes>
+          <Route path="/" element={<HomePage />}/>
+        </Routes>
+      </main>
+    </div>
   )
 }
 
-export default App
+export default App;
