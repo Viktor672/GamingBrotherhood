45bba8e07d0e13b9adfa5a4d76e492a7d1258717
Viktor <149080919+Viktor672@users.noreply.github.com>
2025-03-17 13:29:49 +0200

    Updating App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,26 @@
-import './App.css';
 import { Routes, Route } from 'react-router';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
-
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
-  return (
-    <div id="box">
-      <Header />
+    return (
+        <div className="app-container">
+            <Header />
 
-      <main id="main-content">
-        <Routes>
-          <Route path="/" element={<HomePage />}/>
-        </Routes>
-      </main>
-    </div>
-  )
+            <main className="main-content">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                </Routes>
+            </main>
+
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
