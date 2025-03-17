db48c8b03d44747da8e2084d423d0c53c85cc35b
Viktor <149080919+Viktor672@users.noreply.github.com>
2025-03-17 13:30:56 +0200

    Correcting index.html, Header.jsx and header.css

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,11 +4,11 @@ export default function Header() {
     return (
         <>
             <header>
-                <a href="/" className="brand">Home</a>
+                <Link to="/" className="brand">Home</Link>
                 <div className="menu-btn"></div>
                 <div className="navigation">
                     <div className="navigation-items">
-                        <Link to="/catalog">Games</Link>
+                        <a href="/games">Games</a>
                         <Link to="/about">About</Link>
                         <Link to="/login">Login</Link>
                         <Link to="/register">Register</Link>
