03140c93265e5b0530f0133be5f394555d768168
Viktor <149080919+Viktor672@users.noreply.github.com>
2025-03-17 13:23:19 +0200

    Adding footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import '../styles/footer.css';
+
+export default function Footer() {
+    return (
+        <div className="footer-bottom">
+            <p>© {new Date().getFullYear()} Gaming Brotherhood. All Rights Reserved.</p>
+        </div>
+    )               
+}
