0443722bc282290cce55f4ed7ed07e3949b19ce2
Viktor <viktorsarafov1@abv.bg>
2025-03-21 23:39:04 +0200

    Correct DeletePage.jsx

--- client/src/pages/DeletePage.jsx
+++ client/src/pages/DeletePage.jsx
@@ -1,21 +1,27 @@
 import { useParams } from "react-router";
 import { useDelete } from "../apiHooks/gameApi";
 import { useNavigate } from 'react-router';
+import { useEffect } from "react";
 
 export default function DeletePage() {
     let { deleteGame } = useDelete();
     let navigate = useNavigate();
     let { gameId } = useParams();
 
-    let hasConfirmed = confirm(`Are you sure you want to delete?`);
+    useEffect(() => {
+        let hasConfirmed = window.confirm("Are you sure you want to delete?");
 
-    if (!hasConfirmed) {
-        return;
-    }
+        if (!hasConfirmed) {
+            navigate(`/${gameId}/details`);
+            return;
+        }
 
-    hasConfirmed ? deleteGame(gameId).then(() => {
-        navigate('/games');
-    })
-        :
-        null;
+        deleteGame(gameId).
+        then(() => {
+            return navigate('/games');
+        });
+
+    }, [deleteGame, navigate, gameId]);
+
+    return null;
 }
