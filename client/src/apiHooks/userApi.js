00ff8a49a6ea373949e09342fce41ff8ee1de9d0
Viktor <viktorsarafov1@abv.bg>
2025-03-22 19:49:32 +0200

    Add logic in userApi.js

---
+++ client/src/apiHooks/userApi.js
@@ -0,0 +1,46 @@
+import { useContext } from "react";
+import { handleRequest } from "../utils/handlerequest"
+import { UserContext } from "../contexts/UserContext";
+
+let baseUrl = 'http://localhost:3030/data';
+
+export let useUser = () => {
+    let { accessToken } = useContext(UserContext);
+    let options = {
+        headers: {
+            'X-Authorization': accessToken
+        }
+    }
+
+    let like = async (_ownerId, gameId) => {
+        console.log(_ownerId, gameId);
+
+
+        try {
+            let result = await handleRequest(`${baseUrl}/likes`, 'POST', { _ownerId, gameId }, options);
+            console.log(result);
+
+        }
+        catch (err) {
+            throw new Error(err.message);
+        }
+    }
+
+    let getLikes = async (gameId) => {
+        try {
+            let likesData = await handleRequest(`http://localhost:3030/data/likes`, 'GET');
+
+            likesData = likesData.filter(curObj => curObj.gameId === gameId);
+
+            return likesData.length;
+        } catch (err) {
+            console.error("Error fetching likes:", err);
+            return 0;
+        }
+    }
+
+    return {
+        like,
+        getLikes
+    }
+}
