88a7b4b44db2b53e14f5f0f7b798e37770d144f6
Viktor <viktorsarafov1@abv.bg>
2025-03-27 00:34:48 +0200

    Move Header.jsx component into a new header folder

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,31 @@
+import { useContext } from 'react';
+import { UserContext } from '../../contexts/UserContext';
+import ActiveNavLink from './ActiveLink'; // Import the reusable component
+
+export default function Header() {
+    let { email } = useContext(UserContext);
+
+    return (
+        <header>
+            <ActiveNavLink to="/">Home</ActiveNavLink>
+            <div className="navigation">
+                <div className="navigation-items">
+                    <ActiveNavLink to="/games">Games</ActiveNavLink>
+                    <ActiveNavLink to="/about">About</ActiveNavLink>
+                    {email ? (
+                        <>
+                            <ActiveNavLink to="/create">Create</ActiveNavLink>
+                            <ActiveNavLink to="/logout">Logout</ActiveNavLink>
+                        </>
+                    )
+                        : (
+                            <>
+                                <ActiveNavLink to="/login">Login</ActiveNavLink>
+                                <ActiveNavLink to="/register">Register</ActiveNavLink>
+                            </>
+                        )}
+                </div>
+            </div>
+        </header>
+    );
+}
