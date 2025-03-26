a49fc9bd507a66f596b7a7108c00dee82e794d06
Viktor <viktorsarafov1@abv.bg>
2025-03-27 00:43:03 +0200

    Modifying CreatePage.jsx with useActionState

--- client/src/pages/CreatePage.jsx
+++ client/src/pages/CreatePage.jsx
@@ -1,5 +1,5 @@
-import React, { useContext, useState } from "react";
-import { useCreate } from "../apiHooks/gameApi.js";
+import React, { useActionState, useContext, useEffect, useState } from "react";
+import { useAllGames, useCreate } from "../apiHooks/gameApi.js";
 import { useNavigate } from 'react-router';
 import { UserContext } from "../contexts/UserContext.js";
 
@@ -9,14 +9,44 @@ export default function CreatePage() {
   let { email } = useContext(UserContext);
   let [imageUrl, setImageUrl] = useState('');
 
-  let submitAction = async (formData) => {
-    let gameData = Object.fromEntries(formData);
-    gameData.imageUrl = imageUrl;
-    gameData.authorEmail = email;
-    
-    await create(gameData);
+  let submitHandler = async (state, formData) => {
+    let formDataValues = Object.fromEntries(formData);
+
+    let gameValues = {
+      title: formDataValues.title,
+      genre: formDataValues.genre,
+      description: formDataValues.description,
+      date: formDataValues.date
+    }
+
+    formDataValues.imageUrl = imageUrl;
+    formDataValues.authorEmail = email;
+
+    let data = await create(formDataValues);
+
+    if (data?.error) {
+      alert(data.error);
+      return gameValues;
+    }
 
     navigate('/games');
+  }
+
+  let initialData = {
+    title: '',
+    genre: '',
+    description: '',
+    date: ''
+  }
+
+  let [state, submitAction, isPending] = useActionState(submitHandler, initialData);
+
+  if (isPending) {
+    return (
+      <div className="loading-container">
+        <div className="loading-spinner"></div>
+      </div>
+    );
   }
 
   const handleFileChange = (e) => {
@@ -36,22 +66,22 @@ export default function CreatePage() {
         <form action={submitAction}>
           <div className="input-group">
             <label htmlFor="game-title">Title</label>
-            <input id="game-title" placeholder="Game title" type="text" name="title" className="input-field" />
+            <input id="game-title" placeholder="Game title" type="text" name="title" className="input-field" defaultValue={state.title} />
           </div>
 
           <div className="input-group">
             <label htmlFor="game-genre">Genre</label>
-            <input id="game-genre" placeholder="Action, RPG, FPS" type="text" name="genre" className="input-field" />
+            <input id="game-genre" placeholder="Action, RPG, FPS" type="text" name="genre" className="input-field" defaultValue={state.genre} />
           </div>
 
           <div className="input-group">
             <label htmlFor="game-description">Description</label>
-            <input id="game-description" placeholder="Game Description" type="text" name="description" className="input-field" />
+            <input id="game-description" placeholder="Game Description" type="text" name="description" className="input-field" defaultValue={state.description} />
           </div>
 
           <div className="input-group">
             <label htmlFor="game-release-date">Release Date</label>
-            <input id="game-release-date" type="date" name="date" className="input-field" />
+            <input id="game-release-date" type="date" name="date" className="input-field" defaultValue={state.date} />
           </div>
 
           <div className="input-group">
@@ -60,7 +90,7 @@ export default function CreatePage() {
             {imageUrl && <img src={imageUrl} alt="Cover" className="cover-preview" />}
           </div>
 
-          <button type="submit" className="submit-btn">Submit</button>
+          <button type="submit" className="submit-btn" disabled={isPending}>Submit</button>
         </form>
       </div>
     </div>
