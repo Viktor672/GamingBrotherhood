759228493b1b97b4dd940e7500af88b716610924
Viktor <viktorsarafov1@abv.bg>
2025-03-28 22:51:57 +0200

    Add logoutHandler in LogoutPage.jsx

--- client/src/pages/LogoutPage.jsx
+++ client/src/pages/LogoutPage.jsx
@@ -1,13 +1,14 @@
-import { useNavigate } from "react-router";
-import { useLogout } from "../apiHooks/authApi";
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import { UserContext } from '../contexts/UserContext';
 
 export default function LogoutPage() {
+    let { logoutHandler } = useContext(UserContext);
     let navigate = useNavigate();
-    let { isLogged } = useLogout();
 
-    isLogged
-        ?
-        null
-        :
-        navigate('/')
+    useEffect(() => {
+        logoutHandler();
+        navigate('/');
+    }, [logoutHandler, navigate]);
+
 }
