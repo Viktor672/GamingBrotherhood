e59961843cac03049efb31611f2b86af405af5fd
Viktor <viktorsarafov1@abv.bg>
2025-03-29 02:17:48 +0200

    Add controlled form handling in LoginPage.jsx

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { useActionState, useContext } from 'react';
+import { useActionState, useContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router';
 import { UserContext } from '../contexts/UserContext';
 import { useAuth } from '../apiHooks/authApi';
@@ -7,31 +7,43 @@ export default function LoginPage() {
     let { login } = useAuth();
     let navigate = useNavigate();
     let { setUserHandler } = useContext(UserContext);
+    let [isPending, setIsPending] = useState(false);
+    let [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
 
-    let loginHandler = async (state, formData) => {
-        let formDataValues = Object.fromEntries(formData);
-        let data = await login(formDataValues.email, formDataValues.password);
+    let changeHandler = (e) => {
+        let { name, value } = e.target;
+
+        setFormData(oldState => ({ ...oldState, [name]: value }));
+    }
+
+    let loginHandler = async (e) => {
+        e.preventDefault();
+
+        setIsPending(true);
+        let data = await login(formData.email, formData.password);
+        setIsPending(false);
 
         if (data?.error) {
             alert(data.error);
-            return { email: formDataValues.email };
+            setFormData({ email:formData.email, password: '' });
+            return;
         }
 
         setUserHandler(data);
-        
-        navigate('/games');
-        return { email: formDataValues.email };
-    }
 
-    let [state, loginAction, isPending] = useActionState(loginHandler, { email: '', password: '' });
+        return navigate('/games');
+    }
 
     return (
         <div className="auth-page">
-            <form className="form" action={loginAction}>
+            <form className="form" onSubmit={loginHandler}>
                 <b><p>Login to Your Account</p></b>
                 <br />
-                <input className="auth-input" type="email" id="email" name="email" placeholder="Email" defaultValue={state.email} />
-                <input className="auth-input" type="password" id="password" name="password" placeholder="Password" />
+                <input className="auth-input" type="email" id="email" name="email" placeholder="Email" value={formData.email} onChange={changeHandler} />
+                <input className="auth-input" type="password" id="password" name="password" placeholder="Password" value={formData.password} onChange={changeHandler} />
                 <input className="auth-btn-submit" type="submit" value="Login" disabled={isPending} />
                 <p className="message">
                     Don't have an account? <Link to="/register">Register</Link>
