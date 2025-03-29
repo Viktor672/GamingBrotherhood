ce9f1beb33d2a45d95c6b2cafee1da15fde2d243
Viktor <viktorsarafov1@abv.bg>
2025-03-29 02:17:35 +0200

    Add controlled form handling in RegisterPage.jsx

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,20 +1,38 @@
 import { Link, useNavigate } from 'react-router';
 import { useAuth } from '../apiHooks/authApi';
-import { useActionState, useContext } from 'react';
+import { useActionState, useContext, useState } from 'react';
 import { UserContext } from '../contexts/UserContext';
 
 export default function RegisterPage() {
     let { register } = useAuth();
     let navigate = useNavigate();
     let { setUserHandler } = useContext(UserContext);
+    let [isPending, setIsPending] = useState(false);
+    let [formData, setFormData] = useState({
+        email: '',
+        password: '',
+        rePassword: ''
+    });
 
-    let registerHandler = async (state, formData) => {
-        let formDataValues = Object.fromEntries(formData);
-        let data = await register(formDataValues.email, formDataValues.password, formDataValues.rePassword);
+    let changeHandler = (e) => {
+        let { name, value } = e.target;
 
+        setFormData(oldState => ({ ...oldState, [name]: value }));
+    }
+
+    let registerHandler = async (e) => {
+        e.preventDefault();
+        setIsPending(true);
+        let data = await register(formData.email, formData.password, formData.rePassword);
+        setIsPending(false);
+        
         if (data?.error) {
-            alert(data.error);
-            return { email: formDataValues.email };
+            setFormData({
+                email: formData.email,
+                password: '',
+                rePassword: ''
+            });
+            return alert(data.error);
         }
 
         setUserHandler(data);
@@ -22,16 +40,14 @@ export default function RegisterPage() {
         navigate('/games');
     }
 
-    let [state, registerAction, isPending] = useActionState(registerHandler, { email: '', password: '' });
-
     return (
         <div className="auth-page">
-            <form className="form" action={registerAction}>
+            <form className="form" onSubmit={registerHandler}>
                 <b><p>Register to Your Account</p></b>
                 <br />
-                <input className="auth-input" type="email" id="email" name="email" placeholder="Email" defaultValue={state.email} />
-                <input className="auth-input" type="password" id="password" name="password" placeholder="Password" />
-                <input className="auth-input" type="password" id="rePassword" name="rePassword" placeholder="Repeat Password" />
+                <input className="auth-input" type="email" id="email" name="email" placeholder="Email" value={formData.email} onChange={changeHandler} />
+                <input className="auth-input" type="password" id="password" name="password" placeholder="Password" value={formData.password} onChange={changeHandler} />
+                <input className="auth-input" type="password" id="rePassword" name="rePassword" placeholder="Repeat Password" value={formData.rePassword} onChange={changeHandler} />
                 <input className="auth-btn-submit" type="submit" value="Register" disabled={isPending} />
                 <p className="message">
                     Already have an account? <Link to="/login">Login</Link>
