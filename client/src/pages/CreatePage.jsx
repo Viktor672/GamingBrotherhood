65269edc4db66e76a13aad47548c347490b69fb7
Viktor <viktorsarafov1@abv.bg>
2025-03-29 02:17:06 +0200

    Add controlled form handling in CreatePage.jsx

--- client/src/pages/CreatePage.jsx
+++ client/src/pages/CreatePage.jsx
@@ -1,5 +1,5 @@
-import React, { useActionState, useContext, useEffect, useState } from "react";
-import { useAllGames, useCreate } from "../apiHooks/gameApi.js";
+import React, { useContext, useState } from "react";
+import { useCreate } from "../apiHooks/gameApi.js";
 import { useNavigate } from 'react-router';
 import { UserContext } from "../contexts/UserContext";
 
@@ -7,39 +7,47 @@ export default function CreatePage() {
   let { create } = useCreate();
   let navigate = useNavigate();
   let { email } = useContext(UserContext);
+  let [isPending, setIsPending] = useState(false);
   let [imageUrl, setImageUrl] = useState('');
-
-  let submitHandler = async (state, formData) => {
-    let formDataValues = Object.fromEntries(formData);
-
-    let gameValues = {
-      title: formDataValues.title,
-      genre: formDataValues.genre,
-      description: formDataValues.description,
-      date: formDataValues.date
-    }
-
-    formDataValues.imageUrl = imageUrl;
-    formDataValues.authorEmail = email;
-
-    let data = await create(formDataValues);
-
-    if (data?.error) {
-      alert(data.error);
-      return gameValues;
-    }
-
-    navigate('/games');
-  }
-
-  let initialData = {
+  let [formData, setFormData] = useState({
     title: '',
     genre: '',
     description: '',
     date: ''
+  });
+
+  let changeHandler = (e) => {
+    let { name, value } = e.target;
+    console.log('name', name);
+    console.log('value', value);
+
+    setFormData((oldState) => ({ ...oldState, [name]: value }));
   }
 
-  let [state, submitAction, isPending] = useActionState(submitHandler, initialData);
+  let createHandler = async (e) => {
+    e.preventDefault();
+    setIsPending(true);
+    let newGame = { ...formData, imageUrl, authorEmail: email };
+
+    let data = await create(newGame);
+    setIsPending(false);
+
+    if (data?.error) {
+      alert(data.error);
+      return;
+    }
+
+    navigate('/games');
+  };
+
+  let fileChangeHandler = (e) => {
+    let file = e.target.files?.[0];
+    if (!file) return;
+
+    let reader = new FileReader();
+    reader.onload = (event) => setImageUrl(event.target.result);
+    reader.readAsDataURL(file);
+  };
 
   if (isPending) {
     return (
@@ -49,44 +57,34 @@ export default function CreatePage() {
     );
   }
 
-  const handleFileChange = (e) => {
-    let file = e.target.files?.[0];
-    if (!file) return;
-
-    let reader = new FileReader();
-    reader.onload = (event) => setImageUrl(event.target.result);
-    reader.readAsDataURL(file);
-  };
-
-
   return (
     <div className="form-container">
       <div className="form-box">
         <h1>Add a Game</h1>
-        <form action={submitAction}>
+        <form onSubmit={createHandler}>
           <div className="input-group">
             <label htmlFor="game-title">Title</label>
-            <input id="game-title" placeholder="Game title" type="text" name="title" className="input-field" defaultValue={state.title} />
+            <input id="game-title" placeholder="Game title" type="text" name="title" className="input-field" value={formData.title} onChange={changeHandler} />
           </div>
 
           <div className="input-group">
             <label htmlFor="game-genre">Genre</label>
-            <input id="game-genre" placeholder="Action, RPG, FPS" type="text" name="genre" className="input-field" defaultValue={state.genre} />
+            <input id="game-genre" placeholder="Action, RPG, FPS" type="text" name="genre" className="input-field" value={formData.genre} onChange={changeHandler} />
           </div>
 
           <div className="input-group">
             <label htmlFor="game-description">Description</label>
-            <input id="game-description" placeholder="Game Description" type="text" name="description" className="input-field" defaultValue={state.description} />
+            <input id="game-description" placeholder="Game Description" type="text" name="description" className="input-field" value={formData.description} onChange={changeHandler} />
           </div>
 
           <div className="input-group">
             <label htmlFor="game-release-date">Release Date</label>
-            <input id="game-release-date" type="date" name="date" className="input-field" defaultValue={state.date} />
+            <input id="game-release-date" type="date" name="date" className="input-field" value={formData.date} onChange={changeHandler} />
           </div>
 
           <div className="input-group">
             <label htmlFor="game-cover">Upload Cover</label>
-            <input id="game-cover" name="imageUrl" type="file" accept="image/*" onChange={handleFileChange} className="input-field" />
+            <input id="game-cover" name="imageUrl" type="file" accept="image/*" onChange={fileChangeHandler} className="input-field" />
             {imageUrl && <img src={imageUrl} alt="Cover" className="cover-preview" />}
           </div>
 
