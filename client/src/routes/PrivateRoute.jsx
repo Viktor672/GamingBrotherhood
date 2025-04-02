ba71eabe7d41192d60a960c09aa0f35555cfb07b
Viktor <viktorsarafov1@abv.bg>
2025-04-02 23:08:45 +0300

    Create alert messages in PrivateRoute.jsx and PublicRoute.jsx

--- client/src/routes/PrivateRoute.jsx
+++ client/src/routes/PrivateRoute.jsx
@@ -1,6 +1,17 @@
+import { useRef } from 'react';
 import { Navigate } from 'react-router';
 
 export default function PrivateRoute({ children }) {
+    let hasRunned = useRef(false);
     let isAuthenticated = !!localStorage.getItem('user');
-    return isAuthenticated ? children : <Navigate to="/register" />;
+    if (!isAuthenticated) {
+        if (hasRunned.current) {
+            return <Navigate to="/register" />
+        };
+        hasRunned.current = true;
+        alert("You must be logged in to be able to access this page!");
+    }
+
+
+    return children;
 }
--- client/src/routes/PublicRoute.jsx
+++ client/src/routes/PublicRoute.jsx
@@ -1,6 +1,17 @@
+import { useRef } from 'react';
 import { Navigate } from 'react-router';
 
 export default function PublicRoute({ children }) {
+    let hasRunned = useRef(false);
     let isAuthenticated = !!localStorage.getItem('user');
-    return isAuthenticated ? <Navigate to="/" /> : children;
+
+    if (isAuthenticated) {
+        if (hasRunned.current) {
+            return <Navigate to="/" />
+        };
+        hasRunned.current = true;
+        alert("You are already logged in!");
+    }
+
+    return children;
 }
